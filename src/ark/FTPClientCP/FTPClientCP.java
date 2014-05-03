6f1a250014b3fe74780796529a249a5d6ac91add
Ark <ark6476@gmail.com>
2014-05-03 00:54:20 -0700

    removing singleton stuff. Locking bugs. Refresh config

--- src/ark/FTPClientCP/FTPClientCP.java
+++ src/ark/FTPClientCP/FTPClientCP.java
@@ -9,15 +9,27 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 public class FTPClientCP {
-	private static FTPClientCP instance;
-	private static Object syncRoot = new Object();
+	private Object syncRoot = new Object();
 	private ArrayList<FTPClient> locked, unlocked;
 	private Properties config;
+
+	private boolean blockCheckOut = false;//Used internally if connections need to be called back
 	
 	public FTPClientCP(Properties properties) {
 		locked = new ArrayList<FTPClient>();
 		unlocked = new ArrayList<FTPClient>();
 		this.config = properties;
+	}
+	
+	/**
+	 * Changes the settings. Waits for all threads to be returned first.
+	 * Existing connections are unaffected. Call disconnectAll() to reset them.
+	 * @param newConfig
+	 */
+	public void setProperties(Properties newConfig){
+		synchronized (syncRoot) {//Paranoid...
+			this.config = newConfig;
+		}
 	}
 	
 	public Properties getProperties() {
@@ -28,31 +40,35 @@ public class FTPClientCP {
 		FTPClient client = null;
 		int sleepTimeMS = 500;
 		while (true) {
-			synchronized (syncRoot) {
-				//Try to use an existing connection
-				if (unlocked.size() > 0) {
-					client = unlocked.remove(unlocked.size() - 1);
-					try {
-						client.noop();//Make sure the connection is still alive
-					} catch (IllegalStateException | IOException | FTPIllegalReplyException | FTPException e) {
-						this.disconnect(client);
-						client = null;
+			if(!blockCheckOut){
+				synchronized (syncRoot) {
+					if(blockCheckOut){
+						continue;
 					}
-				}
-				//Try to make a new connection
-				if (null == client) {
-					int maxConnections = Integer.parseInt(config.getProperty("maxConnections"));
-					if (maxConnections < 1) {
-						throw new IllegalStateException("maxConnections " + maxConnections + " must be larger than zero");
+					//Try to use an existing connection
+					if (unlocked.size() > 0) {
+						client = unlocked.remove(unlocked.size() - 1);
+						try {
+							client.noop();//Make sure the connection is still alive
+						} catch (IllegalStateException | IOException | FTPIllegalReplyException | FTPException e) {
+							this.disconnect(client);
+							client = null;
+						}
 					}
-					if (locked.size() + unlocked.size() <= maxConnections) {
-						client = this.create();
+					//Try to make a new connection
+					if (null == client) {
+						int maxConnections = Integer.parseInt(config.getProperty("maxConnections"));
+						if (maxConnections < 1) {
+							throw new IllegalStateException("maxConnections " + maxConnections + " must be larger than zero");
+						}
+						if (locked.size() + unlocked.size() <= maxConnections) {
+							client = this.create();
+						}
 					}
-				}
-				
-				if (null != client) {//We have a connection
-					locked.add(client);
-					return client;
+					if (null != client) {//We have a connection
+						locked.add(client);
+						return client;
+					}
 				}
 			}
 			//Wait for a connection to be returned
@@ -77,7 +93,7 @@ public class FTPClientCP {
 		String password = config.getProperty("password");
 		String server = config.getProperty("server");
 		int port = Integer.parseInt(config.getProperty("port"));
-		boolean passivep = Boolean.parseBoolean(config.getProperty("passive"), "false");
+		boolean passivep = Boolean.parseBoolean(config.getProperty("passive", "false"));
 		
 		if (0 == port) {
 			client.connect(server);
@@ -87,26 +103,61 @@ public class FTPClientCP {
 		}
 		client.login(username, password);
 		client.setPassive(passivep);
-		client.noop();
 		return client;
 	}
 	
-	public void disconnectAll(long timeoutMS) throws InterruptedException, IllegalStateException, IOException, FTPIllegalReplyException, FTPException {
-		long runningTimeMS = 0;
-		long startTS = System.currentTimeMillis();
-		while (unlocked.size() > 0 && runningTimeMS <= timeoutMS) {
-			Thread.sleep(1000);
-			runningTimeMS = System.currentTimeMillis() - startTS;
-		}
-		synchronized (syncRoot) {
-			for (int i = locked.size() - 1; i >= 0; --i) {
-				disconnect(locked.get(i));
+	/**
+	 * Disconnect all clients without any timeout
+	 * @throws FTPException 
+	 * @throws FTPIllegalReplyException 
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 * @throws IllegalStateException 
+	 */
+	public void disconnectAll() throws IllegalStateException, InterruptedException, IOException, FTPIllegalReplyException, FTPException{
+		this.disconnectAll(-1);
+	}
+	
+	/**
+	 * Waits for checked-out connections to return then closes them.
+	 * Forces a close if the timeout is reached. Timeout does nothing if less than zero. 
+	 * @param timeoutMS
+	 * @throws InterruptedException
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 * @throws FTPIllegalReplyException
+	 * @throws FTPException
+	 */
+	public synchronized void disconnectAll(long timeoutMS) throws InterruptedException, IllegalStateException, IOException, FTPIllegalReplyException, FTPException {
+		try{
+			long startTimeMS = System.currentTimeMillis();
+			this.blockCheckOut = true;
+			long runningTimeMS = 0;
+			boolean doTimeout = true;
+			if(timeoutMS < 0){
+				doTimeout = false;
 			}
-			locked = new ArrayList<FTPClient>();
-			for (int i = unlocked.size() - 1; i >= 0; --i) {
-				disconnect(unlocked.get(i));
+			while(true){
+				synchronized(syncRoot){
+					if(locked.size() == 0 || (doTimeout && runningTimeMS <= timeoutMS)){
+						break;
+					}
+				}
+				Thread.sleep(100);
+				runningTimeMS = System.currentTimeMillis() - startTimeMS;
 			}
-			unlocked = new ArrayList<FTPClient>();
+			synchronized (syncRoot) {
+				for (int i = unlocked.size() - 1; i >= 0; --i) {
+					disconnect(unlocked.get(i));
+				}
+				unlocked = new ArrayList<FTPClient>();
+				for (int i = locked.size() - 1; i >= 0; --i) {
+					disconnect(locked.get(i));
+				}
+				locked = new ArrayList<FTPClient>();
+			}
+		}finally{
+			this.blockCheckOut = false;
 		}
 	}
 	
